80acc2c33614c78ed7996bac5374ffd35b9ecf8d
Ma'moun Othman <artofeclipse@gmail.com>
2017-05-13 13:51:38 +0300

    Initial commit to test the new express.js with ES6.

--- server.js
+++ server.js
@@ -1,112 +1,45 @@
-var fs = require('fs');
-var http = require('http');
-var express = require('express');
-var when = require("when");
-var config = require('nconf');
-var swig = require('swig');
-var tools = require('./tools');
-var stationsData = require("./stations-data");
-const querystring = require('querystring');
-var current = {};
-current.date = new Date();
-config.argv().env().file({ file: 'config.json' });
-var app = express();
+let express = require('express');
+let app = express();
+let mongoose = require('mongoose');
+let morgan = require('morgan');
+let bodyParser = require('body-parser');
+let port = 8080;
+let book = require('./app/routes/book');
+let config = require('config'); //we load the db location from the JSON files
+//db options
+let options = { 
+                server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }, 
+                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } 
+              }; 
 
-function buildStationData (stations) {
-    var deferreds = [];
-    current.samples = [];
-    for(var i = 0, len = stations.length; i < len; i++) {
-      var newStation = {
-        id: stations[i][0], 
-        name: stations[i][1],
-        // address: stations[i][2],
-        latitude: stations[i][2],
-        longitude: stations[i][3]
-      };
-      deferreds.push(buildStationSample(newStation).then(function gotIt(sampleData) {
-        current.samples.push(sampleData);
-      }));
-    }
-    return when.all(deferreds);
+//db connection      
+mongoose.connect(config.DBHost, options);
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+//don't show the log when it is test
+if(config.util.getEnv('NODE_ENV') !== 'test') {
+    //use morgan to log at command line
+    app.use(morgan('combined')); //'combined' outputs the Apache style LOGs
 }
 
-function buildStationSample(station) {
-  var deferred = when.defer();
-  var options = {
-      host: 'stations.arabiaweather.com',
-      path: '/weatherstation/api/get?ws='+station.id+'&attr=*',
-      method: 'GET',
+//parse application/json and look for raw text                                        
+app.use(bodyParser.json());                                     
+app.use(bodyParser.urlencoded({extended: true}));               
+app.use(bodyParser.text());                                    
+app.use(bodyParser.json({ type: 'application/json'}));  
 
-  };
-  http.get(options, function(res) {
-      res.setEncoding('utf8');
-      var body = '';
-      res.on('data', function(d) {
-          body += d;
-      });
+app.get("/", (req, res) => res.json({message: "Welcome to our Bookstore!"}));
 
-      res.on('end', function() {
-        try {
-          if(body != 'Not Found') {
-            sampleData = tools.parseToJson(JSON.parse(body));
+app.route("/book")
+    .get(book.getBooks)
+    .post(book.postBook);
+app.route("/book/:id")
+    .get(book.getBook)
+    .delete(book.deleteBook)
+    .put(book.updateBook);
 
-            
-            SampleObj = {
-              stationId: station.id,
-            };
+app.listen(port);
+console.log("Listening on port " + port);
 
-            if(res.statusCode === 200 && typeof sampleData.windspeed != 'undefined') {
-              SampleObj.wv = parseFloat(sampleData.windspeed);
-              SampleObj.wd = tools.cardinalToDegrees(sampleData.winddir);
-            }
-            else {
-              SampleObj.wv = 2;
-              SampleObj.wd = 180;
-            }
-
-            
-          }
-          deferred.resolve(SampleObj);
-          
-        } catch (err) {
-          deferred.reject(new Error('Unable to parse response as JSON' + err));
-        }
-      });
-
-  }).on('error', function(err) {
-    deferred.reject(new Error('Unable to parse response as JSON' + err.message));
-  });
-  return deferred.promise;
-}
-
-function startView() {
-  app.use(express.static(__dirname + "/public"));
-
-  app.engine('html', swig.renderFile);
-
-  app.set('view engine', 'html');
-  app.set('views', __dirname + '/views');
-  app.set('view cache', false);
-
-  swig.setDefaults({ cache: false });
-
-  app.get('/', function(req, res) {
-    res.render('index.html');
-  });
-  app.listen(3000);
-  console.info("Listening on port ..." +  3000);
-}
-
-
-buildStationData(stationsData)
-.then(function pushToCurrent(stations) {
-        fs.writeFileSync(__dirname + '/public/data/current.json', JSON.stringify(current));
-})
-.then(startView());
-
-var interval = setInterval( function() {
-  buildStationData(stationsData)
-  .then(function pushToCurrent(stations) {
-          fs.writeFileSync(__dirname + '/public/data/current.json', JSON.stringify(current));
-  });
-}, 1000 * 60);
+module.exports = app; // for testing
