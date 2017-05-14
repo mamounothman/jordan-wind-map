b9c9c6b69a8225bab3da5db87ac100b227fea44c
mamoun othman <mamounothman@gmail.com>
2017-05-14 17:54:17 +0300

    Added eslint config, added handlebar template engine and some default template.

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+/*eslint no-console: ["error", { allow: ["warn", "error", "log"] }] */
 let express = require('express');
+let exphbs = require('express-handlebars');
 let app = express();
 let mongoose = require('mongoose');
 let morgan = require('morgan');
@@ -8,9 +10,15 @@ let book = require('./app/routes/book');
 let config = require('config'); //we load the db location from the JSON files
 //db options
 let options = { 
-                server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }, 
-                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } 
-              }; 
+    server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }, 
+    replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } 
+}; 
+
+// Register '.mustache' extension with The Mustache Express
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+
+app.set('view engine', 'handlebars');
+// app.set('views', __dirname + '/views');
 
 //db connection      
 mongoose.connect(config.DBHost, options);
@@ -23,23 +31,25 @@ if(config.util.getEnv('NODE_ENV') !== 'test') {
     app.use(morgan('combined')); //'combined' outputs the Apache style LOGs
 }
 
+app.use(express.static('public'));
+
 //parse application/json and look for raw text                                        
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));               
 app.use(bodyParser.text());                                    
 app.use(bodyParser.json({ type: 'application/json'}));  
 
-app.get("/", (req, res) => res.json({message: "Welcome to our Bookstore!"}));
+app.get('/', (req, res) => res.render('home'));
 
-app.route("/book")
+app.route('/book')
     .get(book.getBooks)
     .post(book.postBook);
-app.route("/book/:id")
+app.route('/book/:id')
     .get(book.getBook)
     .delete(book.deleteBook)
     .put(book.updateBook);
 
 app.listen(port);
-console.log("Listening on port " + port);
+console.log('Listening on port ' + port);
 
 module.exports = app; // for testing
