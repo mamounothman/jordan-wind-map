386d375ba0866098fa32ad5da3a0353d45b8a5d2
Ma'moun Othman <artofeclipse@gmail.com>
2017-05-15 16:08:33 +0300

    initial commit for react/redux immplementation of the app.

--- server.js
+++ server.js
@@ -1,101 +1,11 @@
-var fs = require('fs');
-var http = require('http');
-var express = require('express');
-var when = require("when");
-var config = require('nconf');
-var swig = require('swig');
-var tools = require('./tools');
-var stationsData = require("./stations-data");
-var current = {};
-current.date = new Date();
-config.argv().env().file({ file: 'config.json' });
-var app = express();
+let express = require('express');
+let exphbs  = require('express-handlebars');
 
-function buildStationData (stations) {
-    var deferreds = [];
-    current.samples = [];
-    for(var i = 0, len = stations.length; i < len; i++) {
-      var newStation = {
-        id: stations[i][0], 
-        name: stations[i][1],
-        address: stations[i][2],
-        latitude: stations[i][3],
-        longitude: stations[i][4]
-      };
-      deferreds.push(buildStationSample(newStation).then(function gotIt(sampleData) {
-        current.samples.push(sampleData);
-      }));
-    }
-    return when.all(deferreds);
-}
+let app = express();
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 
-function buildStationSample(station) {
-  var deferred = when.defer();
-  http.get({
-    host: 'pro.arabiaweather.com',
-    path: '/data/' + station.name + '/realtime.txt?' + (Math.floor(new Date()/1000))
-  }, function(res) {
-      res.setEncoding('utf8');
-      var body = '';
-      res.on('data', function(d) {
-          body += d;
-      });
+app.use(express.static('public'));
+app.set('view engine', 'handlebars');
 
-      res.on('end', function() {
-        try {
-          sampleData = tools.parseToJson(body);
-          SampleObj = {
-            stationId: station.id,
-          };
-          if(res.statusCode === 200) {
-            SampleObj.wv = parseFloat(sampleData.wspeed) * 0.3;
-            SampleObj.wd = tools.cardinalToDegrees(sampleData.currentwdir);
-          }
-          else {
-            SampleObj.wv = 0;
-            SampleObj.wd = 0;
-          }
-          deferred.resolve(SampleObj);
-        } catch (err) {
-          deferred.reject(new Error('Unable to parse response as JSON' + err));
-        }
-      });
-
-  }).on('error', function(err) {
-    deferred.reject(new Error('Unable to parse response as JSON' + err.message));
-  });
-  return deferred.promise;
-}
-
-function startView() {
-  app.use(express.static(__dirname + "/public"));
-
-  app.engine('html', swig.renderFile);
-
-  app.set('view engine', 'html');
-  app.set('views', __dirname + '/views');
-  app.set('view cache', false);
-
-  swig.setDefaults({ cache: false });
-
-  app.get('/', function(req, res) {
-    res.render('index.html');
-  });
-  app.listen(3000);
-  console.info("Listening on port ..." +  3000);
-}
-
-
-buildStationData(stationsData)
-.then(function pushToCurrent(stations) {
-        fs.writeFileSync(__dirname + '/public/data/current.json', JSON.stringify(current));
-})
-.then(startView());
-
-var interval = setInterval( function() {
-  console.log('output data!!');
-  buildStationData(stationsData)
-  .then(function pushToCurrent(stations) {
-          fs.writeFileSync(__dirname + '/public/data/current.json', JSON.stringify(current));
-  });
-}, 1000 * 60);
+app.get('/', (req, res) => res.render("home"));
+app.listen(3000, () => console.log("Listening to this joint on port 3000!"));
